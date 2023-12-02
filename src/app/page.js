8c48bcb580bc9cf8fbd731b8c80bb0ea1232d3aa
Hamdy Mohamed <121273938+hamdymohamedak@users.noreply.github.com>
2023-12-02 14:33:49 +0200

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -2,32 +2,32 @@
 import { Suspense } from "react";
 import dynamic from "next/dynamic";
 let Homepage = dynamic(() => import("./components/home/page"), {
-  ssr: true,
+  ssr: false,
 });
 let Navbar = dynamic(() => import("./components/navbar/page"), {
-  ssr: true,
+  ssr: false,
 });
 let AboutPage = dynamic(() => import("./components/About/page"), {
-  ssr: true,
+  ssr: false,
 });
 
 let InfoPage = dynamic(() => import("./components/info/page"), {
-  ssr: true,
+  ssr: false,
 });
 
 let Work = dynamic(() => import("./components/work/page"), {
-  ssr: true,
+  ssr: false,
 });
 
 let Footer = dynamic(() => import("./components/footer/page"), {
-  ssr: true,
+  ssr: false,
 });
 let Contact = dynamic(() => import("./components/Contact/page"), {
-  ssr: true,
+  ssr: false,
 });
 
 let Loading = dynamic(() => import("./loading/loading"), {
-  ssr: true,
+  ssr: false,
 });
 
 export default function Home() {
